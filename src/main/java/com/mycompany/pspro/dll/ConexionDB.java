dd9cd6def2ddee0c4b9f1b1c3a0fe7d1273a6c48
Pablo-Miguel <pablomigueldelcastillo@gmail.com>
2022-12-15 20:34:08 +0100

    V3_Fin_Examen

--- src/main/java/com/mycompany/pspro/dll/ConexionDB.java
+++ src/main/java/com/mycompany/pspro/dll/ConexionDB.java
@@ -193,33 +193,41 @@ public class ConexionDB {
         }
     }
     
-    public static DefaultTableModel buildTableModel(String query) throws SQLException {
+    public static DefaultTableModel buildTableModel(String query) {
         
-        enlace();
+        Vector<Vector<Object>> data = null;
+        Vector<String> columnNames = null;
         
-        ResultSet rs = stmt.executeQuery(query);
-        
-        ResultSetMetaData metaData = rs.getMetaData();
-
-        // names of columns
-        Vector<String> columnNames = new Vector<String>();
-        int columnCount = metaData.getColumnCount();
-        for (int column = 1; column <= columnCount; column++) {
-            columnNames.add(metaData.getColumnName(column));
-        }
-
-        // data of the table
-        Vector<Vector<Object>> data = new Vector<Vector<Object>>();
-        while (rs.next()) {
-            Vector<Object> vector = new Vector<Object>();
-            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
-                vector.add(rs.getObject(columnIndex));
+        try {
+            enlace();
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+            
+            ResultSetMetaData metaData = rs.getMetaData();
+            
+            // names of columns
+            columnNames = new Vector<String>();
+            int columnCount = metaData.getColumnCount();
+            for (int column = 1; column <= columnCount; column++) {
+                columnNames.add(metaData.getColumnName(column));
             }
-            data.add(vector);
+
+            // data of the table
+            data = new Vector<Vector<Object>>();
+            while (rs.next()) {
+                Vector<Object> vector = new Vector<Object>();
+                for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++) {
+                    vector.add(rs.getObject(columnIndex));
+                }
+                data.add(vector);
+            }
+            
+            cerrarSesion();
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        cerrarSesion();
-
         return new DefaultTableModel(data, columnNames);
 
     }
@@ -256,42 +264,47 @@ public class ConexionDB {
         }
     }
     
-    public void resultSetToTableModel(String query, JTable table) throws SQLException{
+    public static void resultSetToTableModel(String query, JTable table) {
         
-        enlace();
-        ResultSet rs = stmt.executeQuery(query);
-        
-        //Create new table model
-        DefaultTableModel tableModel = new DefaultTableModel();
-
-        //Retrieve meta data from ResultSet
-        ResultSetMetaData metaData = rs.getMetaData();
-
-        //Get number of columns from meta data
-        int columnCount = metaData.getColumnCount();
-
-        //Get all column names from meta data and add columns to table model
-        for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++){
-            tableModel.addColumn(metaData.getColumnLabel(columnIndex));
-        }
-
-        //Create array of Objects with size of column count from meta data
-        Object[] row = new Object[columnCount];
-
-        //Scroll through result set
-        while (rs.next()){
-            //Get object from column with specific index of result set to array of objects
-            for (int i = 0; i < columnCount; i++){
-                row[i] = rs.getObject(i+1);
+        try {
+            enlace();
+            stmt = conn.prepareStatement(query);
+            rs = stmt.executeQuery();
+            
+            //Create new table model
+            DefaultTableModel tableModel = new DefaultTableModel();
+            
+            //Retrieve meta data from ResultSet
+            ResultSetMetaData metaData = rs.getMetaData();
+            
+            //Get number of columns from meta data
+            int columnCount = metaData.getColumnCount();
+            
+            //Get all column names from meta data and add columns to table model
+            for (int columnIndex = 1; columnIndex <= columnCount; columnIndex++){
+                tableModel.addColumn(metaData.getColumnLabel(columnIndex));
             }
-            //Now add row to table model with that array of objects as an argument
-            tableModel.addRow(row);
+            
+            //Create array of Objects with size of column count from meta data
+            Object[] row = new Object[columnCount];
+            
+            //Scroll through result set
+            while (rs.next()){
+                //Get object from column with specific index of result set to array of objects
+                for (int i = 0; i < columnCount; i++){
+                    row[i] = rs.getObject(i+1);
+                }
+                //Now add row to table model with that array of objects as an argument
+                tableModel.addRow(row);
+            }
+            
+            //Now add that table model to your table and you are done :D
+            table.setModel(tableModel);
+            
+            cerrarSesion();
+        } catch (SQLException ex) {
+            Logger.getLogger(ConexionDB.class.getName()).log(Level.SEVERE, null, ex);
         }
-
-        //Now add that table model to your table and you are done :D
-        table.setModel(tableModel);
-        
-        cerrarSesion();
         
     }
     
